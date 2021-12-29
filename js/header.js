06304eb3250e21a2354848da151a374226daafed
Jehan Kheller <jeh.khe@gmail.com>
2021-12-29 16:01:57 -0400

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,31 +1,33 @@
-const $header = document.querySelector('header')
+const $header = document.querySelector('header');
 
-window.addEventListener('scroll', toggleHeader, false)
+window.addEventListener('scroll', toggleHeader, false);
 
 function toggleHeader() {
+
   if (window.pageYOffset > 75) {
-    $header.classList.add('headerTranslucentColor')
-  } else {
-    $header.classList.remove('headerTranslucentColor')
+    $header.classList.add('headerTranslucentColor');
+  }
+  else {
+    $header.classList.remove('headerTranslucentColor');
   }
 
   if (window.pageYOffset > 425) {
-    $header.classList.remove('headerTranslucentColor')
-    $header.classList.add('headerSolidColor')
-  } else {
-    $header.classList.remove('headerSolidColor')
+    $header.classList.remove('headerTranslucentColor');
+    $header.classList.add('headerSolidColor');
+  }
+  else {
+    $header.classList.remove('headerSolidColor');
   }
 
   //console.log(window.pageYOffset);
 }
 
-/* Menu mobile */
-const menu = document.querySelector('.menu')
-const buttonMenu = document.querySelector('.button-menu')
+// Menu Mobile
+const btnMobile = document.getElementById('btnMenuMobile');
 
-buttonMenu.addEventListener('click', () => {
-  menu.classList.toggle('active')
-  $header.classList.toggle('active-header')
-})
+function toggleMenu(){
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+}
 
-/* ------------- */
+btnMobile.addEventListener('click', toggleMenu);
