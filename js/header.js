1261f1effaf9112c13acea79ad9094f83c8e9307
Lucas Beserra <lucasbeserra932@gmail.com>
2021-12-28 21:29:05 -0300

    responsividade inclusa no header

--- js/header.js
+++ js/header.js
@@ -1,23 +1,31 @@
-const $header = document.querySelector('header');
+const $header = document.querySelector('header')
 
-window.addEventListener('scroll', toggleHeader, false);
+window.addEventListener('scroll', toggleHeader, false)
 
 function toggleHeader() {
-
   if (window.pageYOffset > 75) {
-    $header.classList.add('headerTranslucentColor');
-  }
-  else {
-    $header.classList.remove('headerTranslucentColor');
+    $header.classList.add('headerTranslucentColor')
+  } else {
+    $header.classList.remove('headerTranslucentColor')
   }
 
   if (window.pageYOffset > 425) {
-    $header.classList.remove('headerTranslucentColor');
-    $header.classList.add('headerSolidColor');
-  }
-  else {
-    $header.classList.remove('headerSolidColor');
+    $header.classList.remove('headerTranslucentColor')
+    $header.classList.add('headerSolidColor')
+  } else {
+    $header.classList.remove('headerSolidColor')
   }
 
   //console.log(window.pageYOffset);
 }
+
+/* Menu mobile */
+const menu = document.querySelector('.menu')
+const buttonMenu = document.querySelector('.button-menu')
+
+buttonMenu.addEventListener('click', () => {
+  menu.classList.toggle('active')
+  $header.classList.toggle('active-header')
+})
+
+/* ------------- */
